cd80f938fac8a6e5f5dd77b4574c7bd4c44975ab
ishwant <ishwant08@gmail.com>
2015-05-14 19:44:58 -0700

    budgy-master

---
+++ modules/core/controllers/app.js
@@ -0,0 +1,76 @@
+'use strict'; // <-- what does this mean?
+
+/**
+ * @ngdoc overview
+ * @name budgy
+ * @description
+ * # envelope system
+ *
+ * Main module of the application.
+ */
+ 
+// wrapping your javascript in closure is a good habit
+(function(){
+
+	var app = angular.module('budgyApp', 
+		['ngAnimate',
+		'ngAria',
+		'ngCookies',
+		'ngMessages',
+		'ngResource',
+		'ngRoute',
+		'ngSanitize',
+		'ngTouch',
+		'ui.bootstrap'])
+
+	app.config(function ($routeProvider) {
+		$routeProvider
+		// maybe login page can be integrated into this html file.
+		.when('/', {
+			templateUrl: 'modules/core/views/login.html',
+			controller: 'LoginCtrl'
+		})
+		.when('/lists/new', {
+			templateUrl: 'modules/lists/views/newList.html',
+			controller: 'ListsCtrl'
+		})
+		.when('/lists/:listId', {
+			templateUrl: 'modules/items/views/itemsView.html',
+			controller: 'ItemsCtrl'
+		})
+		// clicking on home will cause main controller to use this route
+		// and will provide the EnvelopesCtrl
+		.when('/envelopes', {
+			templateUrl: 'modules/envelopes/views/budget.html',
+			controller: 'EnvelopesCtrl'
+		})
+		.when('/manageEnvelopes',{
+				templateUrl: 'modules/envelopes/views/manageEnvelopes.html',
+				controller: 'EnvelopesCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	})
+	
+	// controls the behaviour of our navigation bar
+	app.controller('NavbarCtrl', function() {
+		this.tab = 1;
+		
+		this.selectedTab = function(setTab) {
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+	
+	// specify a controller for setting the budgetId:
+	// we could have a login controller set the budgetId & userId 
+	// in the rootScope. Something like the controller below
+	app.controller('LoginCtrl', ['$rootScope', function($rootScope) {
+		$rootScope.budgetId = 1;
+	}]);
+	
+})();
---
+++ modules/envelopes/controllers/envelopesController.js
@@ -0,0 +1,131 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name Budgy App
+ * @description
+ * # Envelope System
+ *
+ * Main module of the application.
+ */
+(function(){
+
+	var app = angular.module('budgyApp');
+
+	app.controller('EnvelopesCtrl', [
+		'$scope', 
+		'$rootScope',
+		'$http', 
+		function($scope,  $rootScope, $http) {
+		
+			$scope.getEnvelopes = function() {
+
+				var url = '/envelopes/' + $rootScope.budgetId;
+			
+				$http.get(url).success(function(data, status, headers, config) {
+					$scope.envelopes = data;
+				});
+			};
+			
+			// proof of concept
+			$scope.udpateEnvelope = function() {
+				
+				var url = '/envelopes/' + $scope.envelopes[0]._id;
+
+				$scope.envelopes[0].amount = 444;
+				
+				$http.put(url, $scope.envelopes[0]).
+					success(function(response, status, headers, config){
+    
+    
+    
+				}).error(function(response, status, headers, config){
+					$scope.error_message = response.error_message;
+				});
+			};
+			
+			// proof of concept
+			$scope.createEnvelope = function() {
+				
+				var url = '/envelopes/';
+				
+				$http.post(url, $scope.envelopes[0]).
+					success(function(response, status, headers, config){
+    
+    
+    
+				}).error(function(response, status, headers, config){
+					$scope.error_message = response.error_message;
+				});
+			};
+			
+			$scope.deleteEnvelope = function() {
+				
+				var url = '/envelopes/' + $scope.envelopes[0]._id;
+				
+				$http.delete(url, $scope.envelopes[0]).
+					success(function(response, status, headers, config){  
+       
+				}).error(function(response, status, headers, config){
+					$scope.error_message = response.error_message;
+				});
+			};
+		}
+	]);
+  
+	app.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+	
+		$scope.items = ['item1', 'item2', 'item3'];
+
+		$scope.animationsEnabled = true;
+
+		$scope.open = function (size) {
+
+			var modalInstance = $modal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'myModalContent.html',
+				controller: 'ModalInstanceCtrl',
+				size: size,
+				
+				resolve: {
+					items: function () {
+						return $scope.items;
+					}
+				}
+			});
+			
+
+		modalInstance.result.then(function (selectedItem) {
+			$scope.selected = selectedItem;
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+
+		$scope.toggleAnimation = function () {
+			$scope.animationsEnabled = !$scope.animationsEnabled;
+		};
+		
+		};
+
+	});
+	
+	// Please note that $modalInstance represents a modal window (instance) dependency.
+	// It is not the same as the $modal service used above.
+
+	app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+	  $scope.items = items;
+	  $scope.selected = {
+		item: $scope.items[0]
+	  };
+
+	  $scope.ok = function () {
+		$modalInstance.close($scope.selected.item);
+	  };
+
+	  $scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	  };
+	});
+
+})();
